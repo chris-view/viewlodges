379dd3c69c3bb720a114f22ed8cd2d6d3d54deda
Samuel Christian <ministerofakidi@gmail.com>
2021-01-04 00:35:07 +0100

    removed youtube iframe

--- src/components/YoutubeVideo.js
+++ src/components/YoutubeVideo.js
@@ -2,12 +2,13 @@ import React from "react";
 
 const youtubeVideo = () =>
 (
-    <iframe width="100%" 
-        height="250em" src="https://www.youtube.com/embed/4_wg6WO9emA?autoplay=1" 
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-        allowFullScreen>
-    </iframe>
+    <h1> Hello from videos </h1>
+    // <iframe width="100%" 
+    //     height="250em" src="https://www.youtube.com/embed/4_wg6WO9emA?autoplay=1" 
+    //     frameBorder="0"
+    //     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+    //     allowFullScreen>
+    // </iframe>
 )
 
 export default youtubeVideo;
