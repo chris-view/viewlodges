7563350c01d781aace6466d3f9decb419e64e340
Samuel Christian <ministerofakidi@gmail.com>
2021-07-08 18:59:15 +0100

    trying online method

--- src/components/GoogleAdBanner.js
+++ src/components/GoogleAdBanner.js
@@ -3,29 +3,17 @@ import PropTypes from 'prop-types';
 
 // const googleAdId = 'ca-pub-7280354271908100';
 
-class GoogleAdBanner extends Component {
-  googleIUnit2 = null;
- 
-
-  componentDidMount() {
-   
-    const { timeout } = this.props;
-    this.googleIUnit2 = setTimeout(() => {
-      if (typeof window !== 'undefined')
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-    }, timeout);
-  }
-
-  componentWillUnmount() {
-    if (this.googleIUnit2) clearTimeout(this.googleIUnit2);
-  }
-
-  render() {
-    const { currentPath } = this.props;
+const GoogleAdBanner = props => {
+    const { currentPath } = props
+    
+    useEffect(() => {
+      window.adsbygoogle = window.adsbygoogle || []
+      window.adsbygoogle.push({})
+    }, [currentPath])
+    
     return (
-      <div key = {currentPath} >
-          
-        <ins 
+      <div key={currentPath}>
+         <ins 
             className="adsbygoogle"
             style={{ display: 'block' }}
             data-ad-client="ca-pub-7280354271908100"
@@ -33,25 +21,8 @@ class GoogleAdBanner extends Component {
             data-ad-format="auto"
             data-full-width-responsive="true">
 
-        </ins>
-          
-          
+         </ins>
       </div>
-    );
+    )
   }
-}
-
-GoogleAdBanner.propTypes = {
- // classNames: PropTypes.string,
-  type: PropTypes.string,
-  timeout: PropTypes.number,
-};
-
-GoogleAdBanner.defaultProps = {
- // classNames: '',
-  timeout: 200,
-//  type: "square",
-};
-
 export default GoogleAdBanner;
-
